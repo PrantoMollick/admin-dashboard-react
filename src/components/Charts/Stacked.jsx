e29fd07f9fdd8def44d1c0b3dc44d93e40814eb5
Pranto Mollick <prantocse01@gmail.com>
2022-06-01 18:20:41 +0600

    stacked chart components with worked

--- src/components/Charts/Stacked.jsx
+++ src/components/Charts/Stacked.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
 
-const Stacked = () => {
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+const Stacked = ({ width, height }) => {
   return (
-    <div>Stacked</div>
-  )
-}
+    <ChartComponent
+      width={width}
+      heigth={height}
+      id="stack charts"
+      // primaryXAxis={stackedPrimaryXAxis}
+      // primaryYAxis={stackedPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      legendSettings={{ background: "white" }}
+    >
+     <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => <SeriesDirective key={index} {...item} />)}
+      </SeriesCollectionDirective>
+    
+    </ChartComponent>
+  );
+};
 
-export default Stacked
+export default Stacked;
